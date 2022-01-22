5e031cefaefc802ff32810b0cda7898339a2ddfa
Bhanu Arora <mrbhanuarora@gmail.com>
2022-01-23 04:48:28 +0530

    try

--- frontend/src/Components/Dashboard/BuyCrops/CropCardBuy/CropCardBuy.jsx
+++ frontend/src/Components/Dashboard/BuyCrops/CropCardBuy/CropCardBuy.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const CropCardBuy = ({ crop }) => {
+  const [transactionData , setTransaction] = useState();
+  const [btnClick , setBtnClick] = useState(false);
+  useEffect(() => {
+    if(btnClick){
+      fetch("http://localhost:8000/create-payment",{
+        method : "POST",
+        
+      })
+    }
+  },[btnClick]);
   return (
     <div className="crop__sell__card flex__center flex__flow__down flex__space__between flex__left">
       <h1>{crop.name}</h1>
