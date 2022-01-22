ce9412290bbd4dedf61fa73a46592264ebef8735
Mohammed Moazzam Khan <kmohdmoazzam70@gmail.com>
2022-01-23 04:00:41 +0530

    Added Setting API

--- backend/api/controller/UserController.js
+++ backend/api/controller/UserController.js
@@ -1,58 +1,66 @@
-const User = require('../model/User')
+const User = require('../model/User');
+const Farmer = require('../model/Farmer');
 const mongoose = require('mongoose');
 const { resetWatchers } = require('nodemon/lib/monitor/watch');
 
-exports.registerUser = async (req, res, next) => {
+exports.registerUser = async(req,res,next) => {
     try {
-        const { name, email, contact, isFarmer, password } = req.body
-
-        var userType;
-        if (isFarmer == "1") userType = true
-        else userType = false
-
-        const userExist = await User.findOne({ email: email });
+        const { name,email,contact,isFarmer,password } = req.body
+        
+        const userExist = await User.findOne({email:email});
 
         if (userExist) {
             res.status(200).send({ status: 400, message: "User already exist!!!" });
             return;
         }
 
+        var userType;
+        if(isFarmer == "1"){
+            userType = true;
+            const newFarmer = new Farmer ({
+                email: email,
+                totalCropSelled: 0,
+                moneyEarned: 0
+            })
+            const farmer = await newFarmer.save();
+        }
+        else userType=false
+
         const createNewUser = new User({
-            name: name,
-            email: email,
-            contact: contact,
+            name:name,
+            email:email,
+            contact:contact,
             isFarmer: userType,
-            password: password
+            password:password,
         })
         const user = await createNewUser.save();
         res.status(200).send({ status: 200, message: "User Registered Successfully" });
         return;
-    } catch (error) {
+    }catch(error) {
         next(error)
     }
 }
 
-exports.loginUser = async (req, res, next) => {
-    try {
+exports.loginUser = async(req,res,next) => {
+    try{
 
-        const { email, password } = req.body
-        const user = await User.findOne({ email: email })
-        const returnUser = await User.findOne({ email: email }).select('-password')
-        if (user == null) {
-            res.status(200).send({ status: 400, message: "Invalid Username or Password" })
-        } else {
-            if (email == user.email && user.password == password) {
+        const {email,password} = req.body
+        const user = await User.findOne({email:email})
+        if(user == null) {
+            res.status(200).send({status:400,message:"Invalid Username or Password"})
+        }else{
+            if(email == user.email && user.password == password) {
                 res.status(200).json({
                     status: 200,
                     message: "User Login Successfully",
-                    data: {
-                        user: returnUser
+                    data : {
+                        user: user
                     }
                 })
             }
         }
 
-    } catch (error) {
+    }catch(error) {
         next(error)
     }
 }
--- backend/api/routes/CropApiRouter.js
+++ backend/api/routes/CropApiRouter.js
@@ -3,11 +3,12 @@ const router = express.Router()
 const cropImage = require("../middleware/cropImage");
 
 
-const { addCrops, cropsDetails, cropById, updateCrop } = require('../controller/CropsController')
+const { addCrops, cropsDetails, cropById, updateCrop, cropByEmail } = require('../controller/CropsController')
 
 router.post('/', cropImage.cropImg, addCrops)
 router.get('/crops', cropsDetails)
 router.get('/:id', cropById)
 router.post('/:id', updateCrop)
+router.post('/cropByEmail', cropByEmail)
 
 module.exports = router
